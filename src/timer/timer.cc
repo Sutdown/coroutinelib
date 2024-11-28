1f6eb90108eee3c876027255267f0cba76cdbc0d
Sutdown <2155197641@qq.com>
2024-11-28 19:43:20 +0800

    test timer

--- src/timer/timer.cc
+++ src/timer/timer.cc
@@ -96,23 +96,68 @@ namespace colib
 
   // 如果条件存在 -> 执行cb()
   static void OnTimer(std::weak_ptr<void> weak_cond, std::function<void()> cb) {
-  
+    std::shared_ptr<void> tmp = weak_cond.lock();
+    if(tmp){
+      cb();
+    }
   }
 
   std::shared_ptr<Timer> TimerManager::addConditionTimer(uint64_t ms, std::function<void()> cb, std::weak_ptr<void> weak_cond, bool recurring){
-    
+    return addTimer(ms, std::bind(&OnTimer, weak_cond, cb), recurring);
   }
 
+  // 找到最近的超时时间
   uint64_t TimerManager::getNextTimer() {
-    
+    std::shared_lock<std::shared_mutex> read_lock(m_mutex);
+
+    // 重置
+    m_tickled = false;
+
+    if(m_timers.empty()){
+      return ~0ull;
+    }
+
+    auto now = std::chrono::system_clock::now();
+    auto time = (*m_timers.begin())->m_next;
+
+    if(now>=time){
+      // 已经超时
+      return 0;
+    }else{
+      // 返回剩余时间
+      auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(time - now);
+      return static_cast<uint64_t>(duration.count());
+    }
   }
 
+  // 取出所有超时定时器的回调函数
   void TimerManager::listExpiredCb(std::vector<std::function<void()>> &cbs) {
-   
+    auto now = std::chrono::system_clock::now();
+    std::unique_lock<std::shared_mutex> write_lock(m_mutex);
+    
+    bool rollover = detectClockRollover(); // 检测时间是否回滚
+
+    // 定时器不为空，rollover为true
+    // 定时器不为空，到达第一个定时器的触发时间
+    while (!m_timers.empty() && rollover || !m_timers.empty() && (*m_timers.begin())->m_next <= now){
+      std::shared_ptr<Timer> temp = *m_timers.begin();
+      m_timers.erase(m_timers.begin());
+
+      cbs.push_back(temp->m_cb);
+
+      if(temp->m_recurring){
+        // 重新加入
+        temp->m_next = now + std::chrono::milliseconds(temp->m_ms);
+        m_timers.insert(temp);
+      }else{
+        temp->m_cb = nullptr;
+      }
+    }
   }
 
   bool TimerManager::hasTimer() {
-    
+    std::shared_lock<std::shared_mutex> read_lock(m_mutex);
+    return !m_timers.empty();
   }
 
   // lock + tickle()
@@ -138,9 +183,13 @@ namespace colib
   bool TimerManager::detectClockRollover() {
     bool rollover = false;
     auto now = std::chrono::system_clock::now();
+    // 如果当前时间 now 小于这个时间窗口的上限，
+    // 说明当前时间比上次记录的时间少了超过一小时，
+    // 这通常是由于系统时间回绕或时钟调整导致的。
     if (now < (m_previouseTime - std::chrono::milliseconds(60 * 60 * 1000))){
       rollover = true;
     }
+    // 调整准确时间
     m_previouseTime = now;
     return rollover;
   }
